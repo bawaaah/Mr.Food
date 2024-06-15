f8540c96b4a8f1f8a0f35cdd7dc34f9cd3b5976e
Ranu20001 <it22168436@my.sliit.lk>
2024-06-15 10:09:56 +0530

    config dotenv

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
 
-const URL = "mongodb+srv://user:1234@cluster0.1o1nvgf.mongodb.net/"
+const URL = process.env.MONGO_URL
 
 
 const connectDB = async () => {
@@ -12,6 +13,6 @@ const connectDB = async () => {
     catch(error){
         console.error('MongoDB connection failed: ', error.message)
     }
-}
+}   
 
 module.exports = connectDB
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express()
 const connectDB = require('./config/db')
+require('dotenv').config()
 
-
+//connect to database
 connectDB()
 
-const port = 3000
 
+const port = process.env.PORT
 //setup app port
 app.listen(port, () => console.log(`App listening to the port ${port}`))
