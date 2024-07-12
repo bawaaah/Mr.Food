62101197db81d990ae5812e7103913a5adb7293c
Ranu20001 <it22168436@my.sliit.lk>
2024-07-12 20:56:06 +0530

    Design admin item page

--- frontend/src/pages/AdminCategory.jsx
+++ frontend/src/pages/AdminCategory.jsx
@@ -16,12 +16,12 @@ function AdminCategory() {
                         <input type="email" name="catName" id="catName" className="form-control" />
                     </div>
                     <div className="form-group my-2">
-                        <label htmlFor="CategoryName" className='text-dark'><strong>Category Description</strong></label>
-                        <textarea name="catDescription" id="" cols="30" rows="10" className="form-control"></textarea>
+                        <label htmlFor="CategoryDescription" className='text-dark'><strong>Category Description</strong></label>
+                        <textarea name="catDescription" id="catDes" cols="30" rows="10" className="form-control"></textarea>
                     </div>
                     <div className="form-group my-2">
                         <label htmlFor="CategoryPhoto"><strong>Category Photo</strong></label><br />
-                        <input type="file" name="" id="" className="form-control-file" />
+                        <input type="file" name="CategoryPhoto" id="catPhoto" className="form-control-file" />
                     </div>
                     <button type="submit" className="btn btn-danger px-4 my-2">Add</button>
                 </form>
